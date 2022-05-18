093357eb090ff21f83eb45c62afdb1595ed55293
Francesco Nisi <48261647+HikaruFN@users.noreply.github.com>
2022-05-18 15:42:30 +0200

    Add files via upload

---
+++ script_for_typed.js
@@ -0,0 +1,20 @@
+var options = {
+  strings: [
+    "Nel prato un fungo vede una funga, e le tira su la gonna. Lei si gira e gli da uno schiaffo dicendogli: “Sei proprio un porcino!",
+    "Da un po’ di giorni non sento più le voci. Domani le chiamo io!",
+    "Madre colpisce il figlio con un ferro da stiro: aveva preso una brutta piega.",
+    "Hanno rubato un tir pieno di lampadine. La polizia brancola nel buio.",
+    "Abbiamo riso abbastanza, adesso pasta.",
+    "Chiude una fabbrica di carta igienica: andava a rotoli.",
+    "Quand’ero piccolo i miei genitori hanno cambiato casa una decina di volte. Ma io sono sempre riuscito a trovarli… (Best One)",
+    "Arrestato un concorso che era stato bandito.",
+  ],
+  typeSpeed: 50,
+  startDelay: 2000,
+  backDelay: 2000,
+  showCursor: true,
+  cursorChar: "|",
+  autoInsertCss: true,
+  shuffle: true,
+};
+var typed = new Typed("#typed_container_text", options);
